b1f815f524558b10fece6e47951ba3f164c9e131
Carol fu <Carol fu>
2016-03-19 13:31:16 -0700

    code not reading a voltage from pin1.0

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../msp430test5.c 
+
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+OBJS += \
+./msp430test5.obj 
+
+C_DEPS += \
+./msp430test5.pp 
+
+OBJS__QTD += \
+".\msp430test5.obj" 
+
+C_DEPS__QTD += \
+".\msp430test5.pp" 
+
+C_SRCS_QUOTED += \
+"../msp430test5.c" 
+
+
